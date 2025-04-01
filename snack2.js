f1c66599734ca50f8b3fa7fcc333830ca7e19f27
ritaurbani <ritaurbani@gmail.com>
2025-04-01 19:36:56 +0200

    updates

--- snack2.js
+++ snack2.js
@@ -1,6 +1,6 @@
 // Crea una arrow function che calcola il quadrato di un numero.
 // Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
 
-const quadrato = (num => num * num)
+const quadrato = num => num * num
 
 quadrato(2)
